9c12573d90fc3f164ebc12953d189096f91484e8
zakarie <zakarie.abdi@student.moringaschool.com>
2025-07-16 15:14:29 +0300

    initial commit

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn(function (isOn) {
+      return !isOn;
+    });
+  }
+
+  let buttonText;
+  if (isOn === true) {
+    buttonText = "ON";
+  } else {
+    buttonText = "OFF";
+  }
+
+  return <button onClick={handleClick}>{buttonText}</button>;
 }
 
 export default Toggle;
